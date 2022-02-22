98d993a569c8f2cce6471745b3edca940df4de4c
Guillem <gcataladordal@gmail.com>
2022-02-22 18:13:26 +0100

    Añadido Coke

--- fullfashion/src/components/Main.js
+++ fullfashion/src/components/Main.js
@@ -2,25 +2,33 @@ import React, { Component } from "react";
 import { Routes, Route } from "react-router-dom";
 import Login from "../pages/Login";
 import Register from "../pages/Register";
+import TipoPersona from "./TipoPersona";
+import AlturaPeso from "./AlturaPeso";
+import Tallas from "./Tallas";
+import Color from "./Color";
+import Estilo from "./Estilo";
 import Mapa from "./Mapa";
-import { Button } from 'react-bootstrap';
-import 'bootstrap/dist/css/bootstrap.min.css';
+import "bootstrap/dist/css/bootstrap.min.css";
 
 class Main extends Component {
-    constructor(props) {
-        super(props);
-    }
-
-    render() {
-        return (
-        <div>
-            <Routes>
-                <Route path="/Login" element={<Login />} />
-                <Route path="/Register" element={<Register />} />
-                <Route path="/Mapa" element={<Mapa />} />
-            </Routes>
-        </div>
-        );
-    }
+  constructor(props) {
+    super(props);
+  }
+  render() {
+    return (
+      <div>
+        <Routes>
+          <Route path="/Login" element={<Login />} />
+          <Route path="/Register" element={<Register />} />
+          <Route path="/Mapa" element={<Mapa />} />
+        </Routes>
+        <TipoPersona />
+        <AlturaPeso />
+        <Tallas />
+        <Color/>
+        <Estilo />
+      </div>
+    );
+  }
 }
 export default Main;
