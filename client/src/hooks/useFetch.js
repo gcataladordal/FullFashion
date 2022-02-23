85db6c07f9b201f8fff1b5dc8919f8a61b0ac649
Lydia Vega <lyvegama@gmail.com>
2022-02-23 16:57:07 +0100

    cambios Coke Lydia

---
+++ client/src/hooks/useFetch.js
@@ -0,0 +1,23 @@
+import { useState, useEffect } from "react";
+import axios from "axios";
+const useFetch = (props) => {
+
+  const [data, setData] = useState("");
+
+
+  useEffect(() => {
+    if (data != "") {
+      searchData(props);
+    }
+  }, [data]);
+
+  const searchData = (props) => {
+    console.log("Pulsa el boton buscar");
+    console.log(data);
+    axios.post(`${props}`, data).then((res) => console.log(res.data));
+  };
+
+  return [data,setData]
+};
+
+export default useFetch;
