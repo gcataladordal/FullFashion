08829e4db5861d3927ade61af93916a5655d2256
Lydia Vega <lyvegama@gmail.com>
2022-02-25 19:10:07 +0100

    contador 6 cambios, eliminar los productos cambiados

--- client/src/hooks/useFetch.js
+++ client/src/hooks/useFetch.js
@@ -10,16 +10,20 @@ const useFetch = (props) => {
     }
   }, [data]);
 
+
   const searchData = (props) => {
     console.log(data);
     axios.post(`${props}`, data).then((res) => {
       localStorage.setItem("resultado", JSON.stringify(res.data));
+      localStorage.setItem("contadorCambios", JSON.stringify(0))
       // saved tiene toda la información ya capada y random (3 de arriba, 2 de abajo y 1 zapato)
       const todoRandom = JSON.parse(localStorage.getItem("resultado"));
       // console.log(todoRandom)
 
     });
   };
+
+
 
   return [data, setData]
 };
