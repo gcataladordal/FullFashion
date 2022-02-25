6dce18223b37d0f301706cd446a1024d7f9f72ec
moramraul <94130850+moramraul@users.noreply.github.com>
2022-02-25 19:11:39 +0100

    Pago guardado en base de datos

--- routes/routes.js
+++ routes/routes.js
@@ -5,28 +5,16 @@ const { required } = require("nodemon/lib/config")
 const products = require("../controllers/product.controllers")
 const user = require("../controllers/users.controllers")
 const Stripe = require("stripe")
+const Pedido =require('../models/compraModel')
+const mongoose = require("mongoose")
+const pago = require("../controllers/compras.controllers")
 
 const stripe = new Stripe("sk_test_51KWzYqAT2Dvvoq4FP3inAGTdnEcI6cQ0lepOWuW8ExJUbOGkvCVqzNx2Cc82Q4xOTw0hUaGeb0algovVt3gI6fSB00NfBV2hGR")
 
 router.post("/register", user.registro)
 router.post("/login", user.loguear)
 
-router.post("/checkout", async (req, res)  => {
-    
-    const { id, amount } = req.body
-
-   const payment = await stripe.paymentIntents.create({
-        amount,
-        currency: "EUR",
-        description: "Pack full fashion",
-        payment_method: id,
-        confirm: true
-
-    })
-    console.log(payment);
-    res.send({message: "Compra realizada correctamente"})
-})
-
+router.post("/checkout", pago) 
 
 router.post("/busquedalook", products.buscarProductos);
 // router.get("/resultadolook/:altura/:color/:estilo/:peso/:talla/:target", function (req,res) {
