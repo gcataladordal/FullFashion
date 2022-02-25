26857c4df3bf0b0fb720a9239e9b3a6c58515b21
Lydia Vega <lyvegama@gmail.com>
2022-02-25 13:52:46 +0100

    check de cambios, falta eliminar del local y devolver el random

--- client/src/pages/ResultadoLook.js
+++ client/src/pages/ResultadoLook.js
@@ -1,51 +1,134 @@
-import React, { Component } from "react";
+import React, { useState } from "react";
 import 'bootstrap/dist/css/bootstrap.min.css';
-import RecogidaLook from "../components/RecogidaLook";
-
+import { Checkbox, Row } from "antd";
 
 function ResultadoLook() {
 
+    const [cambios, setCambios] = useState("");
+
+    // el check 
+    const onChangeCheck = (checkedValues) => {
+        setCambios(checkedValues);
+        // console.log(checkedValues);
+    };
+
+console.log(resultado)
+
+    const recogerCambios = () => {
+        console.log(resultado.todasPartesDeArriba);
+        for (let i = 0; i < resultado.todasPartesDeArriba.length; i++) {
+            if(cambios[0] == resultado.todasPartesDeArriba[i]._id) {
+                console.log(resultado.todasPartesDeArriba[i]._id)
+            } 
+
+            if(cambios[1] == resultado.todasPartesDeArriba[i]._id) {
+                console.log(resultado.todasPartesDeArriba[i]._id)
+            } 
+
+        }
+
+        // console.log(cambios)
+        // console.log(resultado.todasPartesDeArriba.splice(0,1))
+        // console.log(resultado.todasPartesDeArriba.splice(2,1))
+        // console.log(resultado)
+
+    }
+
+    const isDisabled = (id) => {
+
+        return cambios.length > 1 && cambios.indexOf(id) === -1;
+    };
+
+
+    if (localStorage.getItem("resultado")) {
+        var resultado = JSON.parse(localStorage.getItem("resultado"))
+        // console.log(resultado)
+    } else {
+        console.log("No hay datos en el local")
+    }
+    // 0
+    var imagenArriba0 = resultado.todasPartesDeArriba[0].imgUrl
+    // 1
+    var imagenArriba1 = resultado.todasPartesDeArriba[1].imgUrl
+    // 2
+    var imagenArriba2 = resultado.todasPartesDeArriba[2].imgUrl
+    // 3
+    var imagenAbajo0 = resultado.todasPartesDeAbajo[0].imgUrl
+    // 4
+    var imagenAbajo1 = resultado.todasPartesDeAbajo[1].imgUrl
+    // 5
+    var imagenZapatos0 = resultado.todosZapatos[0].imgUrl
+
+    var tituloArriba0 = resultado.todasPartesDeArriba[0].nombre
+    var tituloArriba1 = resultado.todasPartesDeArriba[1].nombre
+    var tituloArriba2 = resultado.todasPartesDeArriba[2].nombre
+
+    var tituloAbajo0 = resultado.todasPartesDeAbajo[0].nombre
+    var tituloAbajo1 = resultado.todasPartesDeAbajo[1].nombre
+
+    var tituloZapatos0 = resultado.todosZapatos[0].nombre
+
+    var idArriba0 = resultado.todasPartesDeArriba[0]._id
+    var idArriba1 = resultado.todasPartesDeArriba[1]._id
+    var idArriba2 = resultado.todasPartesDeArriba[2]._id
+
+    var idAbajo0 = resultado.todasPartesDeAbajo[0]._id
+    var idAbajo1 = resultado.todasPartesDeAbajo[1]._id
+
+    var idZapatos0 = resultado.todosZapatos[0]._id
+
+
     return (
         <div>
-            <form>
+            <Checkbox.Group onChange={(e) => onChangeCheck(e)}  >
+
                 <div>
-                    <img alt="Foto de arriba Full Fashion" src="https://cicadex.com/wp-content/uploads/2018/09/350-858-camisa-Hom-gris-copia-600x600.jpg" width={350} />
-                    <label>Arriba</label>
-                    <input type="checkbox" value="cambiar"></input>
+                    <Checkbox className="color" value={idArriba0} disabled={isDisabled("0")}>
+                        <img alt="Foto de arriba Full Fashion" src={imagenArriba0} width={350} />
+                        <label>{tituloArriba0}</label>
+                    </Checkbox>
                     <br></br>
                 </div>
                 <div>
-                    <img alt="Foto de arriba Full Fashion" src="https://cicadex.com/wp-content/uploads/2018/09/350-858-camisa-Hom-gris-copia-600x600.jpg" width={350} />
-                    <label>Arriba</label>
-                    <input type="checkbox" value="cambiar"></input>
+                    <Checkbox className="color" value={idArriba1} disabled={isDisabled("1")}>
+                        <img alt="Foto de arriba Full Fashion" src={imagenArriba1} width={350} />
+                        <label>{tituloArriba1}</label>
+                    </Checkbox>
                 </div>
                 <div>
-                    <img alt="Foto de arriba Full Fashion" src="https://cicadex.com/wp-content/uploads/2018/09/350-858-camisa-Hom-gris-copia-600x600.jpg" width={350} />
-                    <label>Arriba</label>
-                    <input type="checkbox" value="cambiar"></input>
+                    <Checkbox className="color" value={idArriba2} disabled={isDisabled("2")}>
+                        <img alt="Foto de arriba Full Fashion" src={imagenArriba2} width={350} />
+                        <label>{tituloArriba2}</label>
+                    </Checkbox>
                 </div>
                 <div>
-                    <img alt="Foto de abajo Full Fashion" src="https://www.poloswing.es/1693-thickbox_default/pantalon-cuadros-gales.jpg" width={350} />
-                    <label>Abajo</label>
-                    <input type="checkbox" value="cambiar"></input>
+                    <Checkbox className="color" value={idAbajo0} disabled={isDisabled("3")}>
+                        <img alt="Foto de abajo Full Fashion" src={imagenAbajo0} width={350} />
+                        <label>{tituloAbajo0}</label>
+                    </Checkbox>
                 </div>
                 <div>
-                    <img alt="Foto de abajo Full Fashion" src="https://www.poloswing.es/1693-thickbox_default/pantalon-cuadros-gales.jpg" width={350} />
-                    <label>Abajo</label>
-                    <input type="checkbox" value="cambiar"></input>
+                    <Checkbox className="color" value={idAbajo1} disabled={isDisabled("4")}>
+                        <img alt="Foto de abajo Full Fashion" src={imagenAbajo1} width={350} />
+                        <label>{tituloAbajo1}</label>
+                    </Checkbox>
                 </div>
                 <div>
-                    <img alt="Foto de abajo Full Fashion" src="https://www.puralopez.com/uploads/images/products/zapato-tacon-charol-negro-punta-redonda-pura-lopez-nela1.jpg" width={350} />
-                    <label>Zapatos</label>
-                    <input type="checkbox" value="cambiar"></input>
+                    <Checkbox className="color" value={idZapatos0} id={tituloZapatos0} disabled={isDisabled("5")}>
+                        <img alt="Foto de abajo Full Fashion" src={imagenZapatos0} width={350} />
+                        <label>{tituloZapatos0}</label>
+                    </Checkbox>
+
                 </div>
                 <br></br>
                 <div>
-                    <button>Cambiar las seleccionadas</button>
+                    <button onClick={recogerCambios}>Cambiar las seleccionadas</button>
+
                     <p>*Recuerda que solo tienes 10 cambios de 2 prendas máximo cada vez</p>
                     <button>Seguir para finalizar compra</button>
                 </div>
-            </form>
+            </Checkbox.Group>
+
         </div>
     );
 }
